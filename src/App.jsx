704e88d784831fe7a2894daa654588703eaebbde
Izbassar Nurlan <190103262@stu.sdu.edu.kz>
2024-01-23 23:07:42 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const WallPaper = styled("div")({
   left: 0,
   overflow: "hidden",
   background:
-    "linear-gradient(to right, rgb(97, 80, 143) 0%, rgb(127, 87, 169) 10%, rgb(183, 123, 197) 30%, rgb(275, 229, 247) 100%)", // Brighter colors
+    "linear-gradient(to right, rgb(122, 108, 161) 0%, rgb(150, 116, 186) 10%, rgb(183, 123, 197) 30%, rgb(275, 229, 247) 100%)", // Brighter colors
   transition: "all 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 0s",
   "&::before": {
     content: '""',
@@ -22,7 +22,7 @@ const WallPaper = styled("div")({
     top: "-40%",
     right: "-50%",
     background:
-      "radial-gradient(at center center, rgb(40, 40, 110) 0%, rgba(40, 40, 110, 0) 44%)", // Slightly lighter radial gradient
+      "radial-gradient(at center center, rgb(95, 95, 166) 0%, rgba(40, 40, 110, 0) 44%)", // Slightly lighter radial gradient
   },
   "&::after": {
     content: '""',
